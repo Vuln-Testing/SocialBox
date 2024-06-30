64cb17014b1ba79a9040842cc0e4e64adc40c8fb
MotoAcidic <travisfinch01@gmail.com>
2024-06-30 13:41:32 -0400

    Update links to mine for history purpose

--- install-sb.sh
+++ install-sb.sh
@@ -5,31 +5,31 @@ sudo apt-get update >> /dev/null
 #Python
 sudo apt-get install python tor perl -y
 #gmail
-git clone https://github.com/Ha3MrX/Gemail-Hack.git
+git clone https://github.com/Vuln-Testing/Gemail-Hack.git
 #facebook
 if [ -d facebook ]; then
         echo "Directory facebook exists.[✔]"
 else
 	mkdir facebook
 	cd facebook
-	wget raw.githubusercontent.com/Sup3r-Us3r/scripts/master/fb-brute.pl
+	wget https://raw.githubusercontent.com/Vuln-Testing/scripts/master/fb-brute.pl
 	cd ..
 fi
 #Instagram
-git clone https://github.com/umeshshinde19/instainsane.git
+git clone https://github.com/Vuln-Testing/instainsane.git
 cd instainsane
 chmod +x instainsane.sh
 chmod +x install.sh
 sudo ./install.sh
 cd ..
 #Twitter
-git clone https://github.com/neight-1/-tweetshell-.git
+git clone https://github.com/Vuln-Testing/-tweetshell-.git
 cd ./-tweetshell-
 chmod +x tweetshell.sh
 chmod +x install.sh
 sudo ./install.sh
 cd ..
 # instagram 2
-git clone https://github.com/samsesh/insta-bf.git
+git clone https://github.com/Vuln-Testing/insta-bf.git
 cd insta-bf 
 chmod +x * -R
